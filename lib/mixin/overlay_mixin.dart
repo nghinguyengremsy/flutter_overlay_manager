d5eba4512272f15ddd380f17f1708a1cdfa5b404
nghi.nguyen <nghi.nguyen@inventis.vn>
2024-10-10 15:24:31 +0700

    Insert new entry incorrectly.

--- lib/mixin/overlay_mixin.dart
+++ lib/mixin/overlay_mixin.dart
@@ -52,20 +52,24 @@ mixin OverlayMixin {
     // If the entries is empty, just add new entry to list.
     if (_entries.isEmpty) {
       _entries.add(data);
+      _currentState.insert(data.entry);
     } else {
       // If there's an entry below the new entry, put it behide.
       if (belowOverlayEntry != null) {
         _entries.insert(data.zindex + 1, data);
-        // There's no entry below the new entry, it should be at first in the list.
+        _currentState.insert(
+          data.entry,
+          above: belowOverlayEntry.entry,
+        );
       } else {
+        // There's no entry below the new entry, it should be at first in the list.
         _entries.insert(0, data);
+        _currentState.insert(
+          data.entry,
+          below: _entries.first.entry,
+        );
       }
     }
-    _currentState.insert(
-      data.entry,
-      // below: getEntry(belowOverlayEntry?.below ?? ''),
-      above: belowOverlayEntry?.entry,
-    );
   }
 
   void remove(String id) {
